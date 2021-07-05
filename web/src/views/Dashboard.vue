01ca2b1290811fce2ba67531a098b2b2a2d78307
Joseph Shetaye <josephs@jtstechnic.com>
2021-07-05 15:36:25 -0500

    Fix dashboard server list

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -1,18 +1,20 @@
 <template>
   <v-main>
-    <v-container fill-height fluid>
+    <v-container fluid>
       <v-row dense>
         <v-col
           v-for="server in servers"
           :key="server.id"
-          cols=3>
+          cols="12">
           <v-card
-            :to="`/servers/${server.id}`">
+            :to="`/servers/${server.id}`"
+            width="200"
+          >
             <v-img
               aspect-ratio="1"
-              height="200px"
+              height="200"
               class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.5)"
               :src="getBackgroundString(server.id, server.icon)">
               <v-card-title v-text="server.name" />
             </v-img>
@@ -43,7 +45,7 @@ export default Vue.extend({
       if (icon[0] === "a" && icon[1] === "_") {
         extension = "gif";
       }
-      return `url("https://cdn.discordapp.com/icons/${id}/${icon}.${extension}")`;
+      return `https://cdn.discordapp.com/icons/${id}/${icon}.${extension}`;
     }
   },
   mounted() {
